9d55186871edba3de1490a2e2eb82bc58b7f41fb
D1360-64RC14 <gdiego416@gmail.com>
2021-10-16 14:12:15 -0300

    Adicionado LUT a algumas funções

--- utils/ConvolutionEncoding/ConvolutionEncoding.go
+++ utils/ConvolutionEncoding/ConvolutionEncoding.go
@@ -66,14 +66,26 @@ func (ce *ConvolutionEncoding) SendBit(bit byte) []byte {
 // bitFill(4) = 0b1111
 //
 // bitFill(5) = 0b11111
-func bitFill(length uint8) (result byte) {
-	result = 0
+func bitFill(length uint8) byte {
+	// result = 0
 
-	for i := uint8(0); i < length; i++ {
-		result = result | 1 << i
+	// for i := uint8(0); i < length; i++ {
+	// 	result = result | 1 << i
+	// }
+
+	// return
+	LUT := []byte{
+		0b00000000,
+		0b00000001,
+		0b00000011,
+		0b00000111,
+		0b00001111,
+		0b00011111,
+		0b00111111,
+		0b01111111,
+		0b11111111,
 	}
-
-	return
+	return LUT[length]
 }
 
 // readBitPosition lê um bit em uma posição específica do byte.
@@ -82,17 +94,33 @@ func bitFill(length uint8) (result byte) {
 // readBitPosition(0b101, 1) = 0 // 0b1[0]1
 //
 // readBitPosition(0b10011101, 3) = 1 // 0b1001[1]101
-func readBitPosition(data byte, position uint8) byte {
-	return 0b1 & (data >> position)
-}
+// func readBitPosition(data byte, position uint8) byte {
+// 	return 0b1 & (data >> position)
+// }
 
 // sumByte efetua uma soma entre todos os bits de um byte.
 // Por exemplo:
 //
 // sumByte(0b101001) = 1 + 0 + 1 + 0 + 0 + 1 = 1
-func sumByte(data byte) (result byte) {
-	for i := uint8(0); i < 8; i++ {
-		result = result + readBitPosition(data, i)
+func sumByte(data byte) byte {
+	// for i := uint8(0); i < 8; i++ {
+	// 	result = result + readBitPosition(data, i)
+	// }
+	// return 0b1 & result
+	LUT := []byte{
+		0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1,
+		0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0,
+		0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1,
+		0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1,
+		0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0,
+		1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0,
+		0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0,
+		1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0,
+		0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1,
+		0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1,
+		1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0,
+		1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1,
+		0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1,
 	}
-	return 0b1 & result
+	return LUT[data]
 }
--- utils/ConvolutionEncoding/ConvolutionStep.go
+++ utils/ConvolutionEncoding/ConvolutionStep.go
@@ -1,5 +1,7 @@
 package convolutionencoding
 
+// ConvolutionStep funciona com: dado um buffer, próximo bit, polinômios e quantidade de bits (k),
+// executa as operações e retorna os devidos valores.
 func ConvolutionStep(buffer *byte, nextBit byte, polinomials []byte, k int) []byte {
 	nextBit &= 1
 
@@ -10,20 +12,35 @@ func ConvolutionStep(buffer *byte, nextBit byte, polinomials []byte, k int) []by
 	return andSumPolynomials(buffer, &polinomials)
 }
 
+// limitPolinomialsByK limita o tamanho dos polinômio de acordo com a quantidade de bits (k).
 func limitPolinomialsByK(polynomials *[]byte, limit int) {
 	for index := range *polynomials {
 		(*polynomials)[index] &= byteFill(limit)
 	}
 }
 
-// can be made by LUT
-func byteFill(qnt int) (result byte) {
-	for i := 0; i < qnt; i++ {
-		result |= 1 << i
+// byteFill preenche um byte com bits 1 de acordo com a quantidade informada.
+// Feito utilizando LUT.
+func byteFill(qnt int) byte {
+	// for i := 0; i < qnt; i++ {
+	// 	result |= 1 << i
+	// }
+	// return
+	LUT := []byte{
+		0b00000000,
+		0b00000001,
+		0b00000011,
+		0b00000111,
+		0b00001111,
+		0b00011111,
+		0b00111111,
+		0b01111111,
+		0b11111111,
 	}
-	return
+	return LUT[qnt]
 }
 
+// andSumPolynomials executa as operações retornando o resultado final.
 func andSumPolynomials(buffer *byte, polinomials *[]byte) (result []byte) {
 	result = make([]byte, len(*polinomials))
 
@@ -34,10 +51,27 @@ func andSumPolynomials(buffer *byte, polinomials *[]byte) (result []byte) {
 	return
 }
 
-// can be made by LUT
-func xorByte(data byte) (result byte) {
-	for i := 0; i < 8; i++ {
-		result ^= 1 & (data >> i)
+// xorByte soma todos os bits de um byte.
+// Feito utilizando LUT.
+func xorByte(data byte) byte {
+	// for i := 0; i < 8; i++ {
+	// 	result ^= 1 & (data >> i)
+	// }
+	// return
+	LUT := []byte{
+		0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1,
+		0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0,
+		0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1,
+		0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1,
+		0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0,
+		1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0,
+		0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0,
+		1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0,
+		0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1,
+		0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1,
+		1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0,
+		1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1,
+		0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1,
 	}
-	return
+	return LUT[data]
 }
